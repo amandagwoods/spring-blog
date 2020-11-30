dfe07a325904de76b7e678cc099a765b9d47d64e
Amanda <amanda.garrison.woods@gmail.com>
2020-11-30 12:15:24 -0600

    feat: completed postController exercise

--- src/main/java/com/codeup/blog/controllers/PostController.java
+++ src/main/java/com/codeup/blog/controllers/PostController.java
@@ -1,24 +1,50 @@
 package com.codeup.blog.controllers;
 
+import com.codeup.blog.Post;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class PostController {
 
-    @GetMapping("/posts")
-    @ResponseBody
-    public String index() {
-        return "posts index page";
+//    @GetMapping("/posts")
+//    @ResponseBody
+//    public String index() {
+//        return "posts index page";
+//    }
+
+    @GetMapping("/posts/index")
+    public String index(Model model){
+        List<String> posts = new ArrayList<>();
+        posts.add("This is the first post");
+        posts.add("This is the second post");
+        model.addAttribute("posts", posts);
+        return "/posts/index";
     }
 
-    @GetMapping("/posts/{id}")
-    @ResponseBody
-    public String show(@PathVariable long id) {
-        return "view individual post " + id;
+//    @GetMapping("/posts/{id}")
+//    @ResponseBody
+//    public String show(@PathVariable long id) {
+//        return "view individual post " + id;
+//    }
+
+    @GetMapping("/posts/show/{id}")
+    public String showIndividualPost(@PathVariable long id, Model model) {
+        Post post = new Post();
+        String title = "This is a title";
+        String body = "This is the body of post";
+        model.addAttribute("id", "Post id#: " + id);
+        model.addAttribute("title", title);
+        model.addAttribute("body", body);
+        return "/posts/show";
     }
 
     @GetMapping("/posts/create")
