e4ef825361ef7636d203bc52d807081a19823ee1
Amanda <amanda.garrison.woods@gmail.com>
2020-11-29 23:56:28 -0600

    view lesson

--- src/main/java/com/codeup/blog/controllers/HomeController.java
+++ src/main/java/com/codeup/blog/controllers/HomeController.java
@@ -1,8 +1,14 @@
 package com.codeup.blog.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class HomeController {
@@ -16,5 +22,22 @@ public class HomeController {
     @GetMapping("/home")
     public String welcome(){
         return "home";
+    }
+
+    @GetMapping("/join")
+    public String showJoinForm(Model model){
+        List<String> cohortNames = new ArrayList<>();
+        cohortNames.add("Bash");
+        cohortNames.add("Apex");
+        cohortNames.add("Io");
+
+        model.addAttribute("cohortNames", cohortNames);
+        return "/join";
+    }
+
+    @PostMapping("/join")
+    public String postJoinForm(@RequestParam(name = "cohort") String cohort, Model model){
+        model.addAttribute("cohort", "Welcome to " + cohort + "!");
+        return "/join";
     }
 }
