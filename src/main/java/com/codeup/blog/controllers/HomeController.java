25400b3c0f8e2b9dd67f5f692353e652225a7543
Amanda <amanda.garrison.woods@gmail.com>
2020-12-07 12:37:49 -0600

    feat: working through views lecture

--- src/main/java/com/codeup/blog/controllers/HomeController.java
+++ src/main/java/com/codeup/blog/controllers/HomeController.java
@@ -1,7 +1,9 @@
 package com.codeup.blog.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -9,8 +11,14 @@ public class HomeController {
 
     @GetMapping("/")
     @ResponseBody
-    public String landingpage(){
+    public String landingPage(){
         return "This is the landing page!";
     }
+
+    @GetMapping("/home")
+    public String welcome(){
+        return "home";
+    }
+
 
 }
